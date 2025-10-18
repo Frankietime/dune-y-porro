ffe32d2b92c111eb4a8626b7a36aa68cea2f08ff
Frankietime <donadiofranco7@gmail.com>
2025-10-17 20:29:58 -0600

    - game cycle - new libraries (i.e.: ReactQuery) - shared workspace - services - context wrappers

--- client/src/components/board-component/BoardComponent.tsx
+++ client/src/components/board-component/BoardComponent.tsx
@@ -1,9 +1,170 @@
-import board from "../../assets/board/board.png"
+import { useState } from "react";
+import mapBg from "../../assets/map-full.png";
+import { useAppStore } from "../../store";
+import { useBoardComponent } from "./UseBoardComponent";
+import { useQuery } from "@tanstack/react-query";
+import { useLobbyServices } from "../services/lobbyServices";
+import { BoardProps } from "boardgame.io/react";
+import { GameState } from "../../../../shared/types";
 
-export const BoardComponent = () => {
+interface BoardGameProps extends BoardProps<GameState> {};
 
+export const BoardComponent = ({ 
+    ctx, 
+    G, 
+    moves, 
+    events 
+}: BoardGameProps) => {
 
-    return (
-        <img src={board}></img>
-    )
+  const [errorNotification, setErrorNotification] = useState("");
+  
+  const {
+    getMatch,
+  } = useLobbyServices();
+
+  const {
+    playerProps,
+    setPlayerProps,
+
+    matchData,
+    setMatchData
+  } = useAppStore();
+
+  const {
+    Hud,
+    Clickers,
+    NumericTrackers,
+    NumericTracker,
+    VisualTracker,
+    DynamicElement
+  } = useBoardComponent();
+
+  useQuery({
+    queryKey: ["fetch-match-data"], 
+    queryFn: () => getMatch(playerProps.matchID)    
+      .then((match) => {
+          setMatchData(match);
+      },
+      (error) => {
+        setErrorNotification(error)
+      }
+    ),
+    enabled: playerProps != null && playerProps.matchID != ""}
+  );
+
+  const onLeaveMatch = async () => {
+    leaveMatch(playerProps)
+      .then(() => {
+        setPlayerProps({ 
+          ...playerProps, 
+          matchID: "", 
+          playerCredentials: ""
+        });
+    });
+  };
+
+  const onTurnEnd = (): void => {
+    events.endTurn!();
+  }
+
+return (
+  <div className='game-container'>
+    {matchData != null && matchData.matchID && (
+      <>
+        <div className="absolute top-6 left-4 rounded-xl bg-black/60 px-6 py-4 text-base text-gray-100 shadow-lg backdrop-blur-sm">
+          <h1 className="font-semibold text-lg">
+            Game | {matchData.setupData.name}
+          </h1>
+
+          <div className="mt-2 space-y-1.5 leading-snug">
+            <div>
+              <span className="font-medium text-gray-200">Player Name:</span>{" "}
+              <span className="text-gray-300">{playerProps.name}</span>
+            </div>
+             <div>
+              <span className="font-medium text-gray-200">Players:</span>{" "}
+              <span className="text-gray-300">
+                {
+                  matchData.players.map((p: any, index: number) => <p key={index} className={ctx.currentPlayer == p.id ? 'underline' : ''}>{p.name}</p> )
+                }
+              </span>
+            </div>
+            <div>
+              <span className="font-medium text-gray-200">ID:</span>{" "}
+              <span className="text-gray-300">{playerProps.playerID}</span>
+            </div>
+            <div>
+              <span className="font-medium text-gray-200">Match ID:</span>{" "}
+              <span className="text-gray-300">{playerProps.matchID}</span>
+            </div>
+            <div>
+              <span className="font-medium text-gray-200">Creds:</span>{" "}
+              <span className="text-gray-300">{playerProps.playerCredentials}</span>
+            </div>
+
+            {errorNotification != "" && errorNotification != null && (
+              <div>
+                <span className="text-red-300">{errorNotification}</span>
+              </div>
+            )}
+          </div>
+
+          <button
+            onClick={onLeaveMatch}
+            className="mt-4 rounded-md bg-red-600 px-4 py-2 text-sm font-medium text-white shadow-sm transition hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500"
+          >
+            Leave
+          </button>
+        </div>
+
+        <div className="flex w-screen justify-around board">
+          <div className="w-[1280px] h-[720px] relative border-1 box-content" style={{
+            backgroundImage: `url(${mapBg})`,
+          }}>
+            <NumericTrackers x={277} y={88} /> {/* vertical trackers */}
+            <Clickers x={355} y={67} /> {/* red clickers */}
+            <NumericTracker x={345} y={209} w={54} h={54} show={true} />  {/* side circular tracker */}
+            <VisualTracker x={400} y={210} show={true} /> {/* purple visual tracker */}
+            <NumericTracker x={575} y={190} w={80} h={75} show={true} />  {/* square numeric tracker combat */}
+            <DynamicElement x={229} y={197} show={true} />  {/* green dynamic elem */}
+
+            <NumericTrackers x={989} y={88} />
+            <Clickers x={613} y={67} mirror={true} />
+            <NumericTracker x={912} y={209} w={54} h={54} show={true} />
+            <VisualTracker x={735} y={210} show={true} />
+            <NumericTracker x={657} y={190} w={80} h={75} show={true} />
+            <DynamicElement x={1045} y={197} show={true} />
+
+            <NumericTrackers x={277} y={279} />
+            <Clickers x={303} y={344} mirror={true} />
+            <NumericTracker x={345} y={270} w={54} h={54} show={true} />
+            <VisualTracker x={400} y={270} show={true} />
+            <NumericTracker x={575} y={270} w={80} h={75} show={true} />
+            <DynamicElement x={281} y={463} show={true} />
+
+            <NumericTrackers x={989} y={279} />
+            <Clickers x={665} y={344} />
+            <NumericTracker x={912} y={270} w={54} h={54} show={true} />
+            <VisualTracker x={735} y={270} show={true} />
+            <NumericTracker x={657} y={270} w={80} h={75} show={true} />
+            <DynamicElement x={1045} y={385} show={true} />
+            
+            <Hud 
+              onPass={() => onTurnEnd()}
+              onReveal={() => alert("Reveal!")}
+              onSelectCard={(i) => alert(`Selected card ${i}`)}
+              onArrowUp={() => alert("arrow up")}
+              onArrowDown={() => alert("arrow down")}
+            />
+          </div>
+        </div>
+      </>
+      
+    )}
+
+    </div>
+
+    
+    
+  );
 }
--- client/src/components/location-component/LocationComponent.tsx
+++ client/src/components/location-component/LocationComponent.tsx
@@ -1,77 +1,72 @@
-import { Box, Flex, Grid } from "@radix-ui/themes";
-import water from "../../assets/icons/c-water.png";
-import triangle from "../../assets/icons/loc-triang.png";
-import deploy from "../../assets/icons/b-deploy.png";
-import "./LocationComponent.scss";
-import { Location, Resource, ResourceEnum } from "../../store";
-import React from "react";
+// import "./LocationComponent.scss";
+// import React from "react";
 
-type LocationProps = {
-    location: Location; 
-    resources: Resource[];
-    position?: { top: number; right: number; }
-    onSelect: (callback: () => void) => void;
-}
+// type LocationProps = {
+//     location: Location; 
+//     resources: [];
+//     position?: { top: number; right: number; }
+//     onSelect: (callback: () => void) => void;
+// }
 
 
-export const LocationComponent = ({
-    location, 
-    resources, 
-    position,
-    onSelect
-} : LocationProps) => {
+// export const LocationComponent = ({
+//     location, 
+//     resources, 
+//     position,
+//     onSelect
+// } : LocationProps) => {
 
-    const [alreadySelected, setAlreadySelected] = React.useState(false);
+//     const [alreadySelected, setAlreadySelected] = React.useState(false);
 
-    const hasEnoughResources = () => {
-        return resources.find(res => res.type == location.cost.resource.type && res.amount >= location.cost.resource.amount)
-    };
+//     const hasEnoughResources = () => {
+//         return resources.find(res => res.type == location.cost.resource.type && res.amount >= location.cost.resource.amount)
+//     };
 
-    const styles = {
-        top: position?.top ?? 0,
-        right: position?.right ?? 0,
-        // bottom: 500
-    }
+//     const styles = {
+//         top: position?.top ?? 0,
+//         right: position?.right ?? 0,
+//         // bottom: 500
+//     }
 
-    return (
-        <Grid 
-            key={location.index}
-            className="location-container" 
-            columns="3" rows="repeat(1, 64px)" 
-            style={alreadySelected ? { ...styles, opacity: 0.5 } : {...styles}} 
-            onClick={() => {
-                if (alreadySelected || !hasEnoughResources()) return; 
-                onSelect(() => { setAlreadySelected(true); });
-            }}
-        >
-            {/* <p className="title">{location.name}</p>
-            <div className="location-section">
-                <Box>
-                    {location.cost.resource.type}: {location.cost.resource.amount}
-                </Box>                
-                <Box >
-                    <Flex gap="1">
+//     return (
+//         <Grid 
+//             key={location.index}
+//             className="location-container" 
+//             columns="3" rows="repeat(1, 64px)" 
+//             style={alreadySelected ? { ...styles, opacity: 0.5 } : {...styles}} 
+//             onClick={() => {
+//                 if (alreadySelected || !hasEnoughResources()) return; 
+//                 onSelect(() => { setAlreadySelected(true); });
+//             }}
+//         >
+//             {/* <p className="title">{location.name}</p>
+//             <div className="location-section">
+//                 <Box>
+//                     {location.cost.resource.type}: {location.cost.resource.amount}
+//                 </Box>                
+//                 <Box >
+//                     <Flex gap="1">
                         
-                    </Flex>
-                </Box> 
-            </div>
+//                     </Flex>
+//                 </Box> 
+//             </div>
 
-            <div className="location-section">
-                <Box >
-                    <img className="location-card-type-icon" src={triangle} alt="card-type"></img>
-                </Box>                
-                <Box >
-                    <Flex gap="1">
+//             <div className="location-section">
+//                 <Box >
+//                     <img className="location-card-type-icon" src={triangle} alt="card-type"></img>
+//                 </Box>                
+//                 <Box >
+//                     <Flex gap="1">
                         
-                    </Flex>
-                </Box> 
-            </div>
+//                     </Flex>
+//                 </Box> 
+//             </div>
             
-            <div className="location-section location-deploy-section">
-                <Box >
-                    <img className="location-card-type-icon" src={deploy} alt="card-type"></img>
-                </Box>                
-            </div> */}
-       </Grid>        
-    );
-}
+//             <div className="location-section location-deploy-section">
+//                 <Box >
+//                     <img className="location-card-type-icon" src={deploy} alt="card-type"></img>
+//                 </Box>                
+//             </div> */}
+//        </Grid>        
+//     );
+// }
